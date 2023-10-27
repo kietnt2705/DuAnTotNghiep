5e8cbf73f693be8fa5078164eea4901095b439be
GamingPro <GamingPro@LAPTOP-R5D6HVIO>
2023-10-27 16:54:11 +0700

    27/10/2023 - giao diện cơ bản

---
+++ src/main/java/com/project/controller/opencontroller.java
@@ -0,0 +1,28 @@
+package com.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.bean.Producttype;
+import com.project.dao.Producttype_DAO;
+
+@Controller
+@RequestMapping("/trangchu")
+public class opencontroller {
+	@GetMapping("/admin")
+	public String admin() {
+		return "/Admin/index";
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return "/User/index";
+	}
+}
